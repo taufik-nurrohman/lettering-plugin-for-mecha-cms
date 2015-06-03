7cd9d5032b7e159a71e9de7b8114b25af2b3ef6e
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-06-03 20:07:06 +0700

    Cleaned Up by the CMS Core
    https://github.com/mecha-cms/mecha-cms/commit/7678114214f840060c2149d2e150df681297e0d1

--- lettering-php/__launch.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Cleaning up ...
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if(isset($_POST['fields']['break_title_text']['type']) && ( ! isset($_POST['fields']['break_title_text']['value']) || isset($_POST['fields']['break_title_text']['value']) && $_POST['fields']['break_title_text']['value'] === false)) {
-        unset($_POST['fields']['break_title_text']);
-    }
-}
